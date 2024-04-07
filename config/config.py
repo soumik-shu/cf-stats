177fed0eaa39728ff7f7b830d6cfc18aa37cabf4
Md. Samiul Islam Soumik <148578996+soumik-shu@users.noreply.github.com>
2024-04-07 21:36:01 +0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     """Settings class for defining all variables."""
-    cf_handle: str = 'DmitriyH'
+    cf_handle: str = 'Soumik_SHU'
     base_dir: str = str(Path(__file__).parent.parent)
     acronym_ignore: list = ['and']
     max_name_len: int = 13  # defines the max len of a cf user's full name, default 13
